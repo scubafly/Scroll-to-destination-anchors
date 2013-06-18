6b3904c49b3bf075fff0b16be7b36de243577641
Keegan Robinson <keeganhr@gmail.com>
2013-06-18 13:54:26 -0400

    By keeganhr, Keegan: Adjusting code formatting to match Drupal standards.

--- scroll_to_destination_anchors.js
+++ scroll_to_destination_anchors.js
@@ -1,40 +1,20 @@
 (function($) {
-	Drupal.behaviors.scrolltoanchors = {
-		attach: function(context, settings) {
-			
-			// when the document is ready
-			$(document).ready(function(){
 
-				// select links that start with a hash tag
-				$('a[href^="#"]').click(function(event) {
+Drupal.behaviors.scrolltoanchors = {
+  attach: function(context, settings) {
+    $(document).ready(function(){
+      $('a[href^="#"]').click(function(event) {
+        event.preventDefault();
+        var destination = 0;
+        if ( $(this.hash).offset().top > $(document).height() - $(window).height() ) {
+          destination = $(document).height() - $(window).height();
+        } else {
+          destination = $(this.hash).offset().top;
+        }
+        $('html,body').animate({ scrollTop: destination }, 800, 'swing');
+      });
+    });
+  }
+};
 
-					// prevent default behavior
-					event.preventDefault();
-
-					// declare destination variable	
-					var destination = 0;
-
-					// if the link is down the page
-					if ( $(this.hash).offset().top > $(document).height() - $(window).height() ) {
-
-						// set destination to distance from the top of the window
-						destination = $(document).height() - $(window).height();
-
-					// if the link is not down the page
-					} else {
-
-						// set the destination to the top of the page
-						destination = $(this.hash).offset().top;
-
-					}
-
-					// scroll to destination
-					$('html,body').animate({ scrollTop: destination }, 800, 'swing');
-
-				});
-				
-			});
-			
-		}
-	};
 }(jQuery));
