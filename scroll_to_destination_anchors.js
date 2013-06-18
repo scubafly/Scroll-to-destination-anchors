bab2261f4be9912032c57283e8dd276af1169ff8
Keegan Robinson <keeganhr@gmail.com>
2013-06-18 11:38:41 -0400

    Issue #0 by keeganhr, Keegan: Adding module files.

---
+++ scroll_to_destination_anchors.js
@@ -0,0 +1,36 @@
+(function ($) {
+	Drupal.behaviors.scrolltoanchors = {
+		attach: function (context, settings) {
+
+			// select links that start with a hash tag
+			$('a[href^="#"]').click(function(event){
+
+			// prevent default behavior
+			event.preventDefault();
+
+			// declare destination variable	
+			var destination = 0;
+
+			// if the link is down the page
+			if($(this.hash).offset().top > $(document).height()-$(window).height()){
+
+				// set destination to distance from the top of the window
+				destination = $(document).height()-$(window).height();
+
+			// if the link is not down the page
+			} else {
+
+				// set the destination to the top of the page
+				destination = $(this.hash).offset().top;
+
+			}
+
+				// scroll to destination
+				$('html,body').animate({scrollTop:destination}, 1000,'swing');
+
+			});
+
+		}
+	};
+}(jQuery));
+
