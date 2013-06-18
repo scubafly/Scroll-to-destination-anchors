c5af13c00b884bc6374a8e836b9b884b20417fca
Keegan Robinson <keeganhr@gmail.com>
2013-06-18 11:53:29 -0400

    Issue #0 by keeganhr, Keegan: Fixed inconsistent script firing."

--- scroll_to_destination_anchors.js
+++ scroll_to_destination_anchors.js
@@ -1,36 +1,40 @@
-(function ($) {
+(function($) {
 	Drupal.behaviors.scrolltoanchors = {
-		attach: function (context, settings) {
+		attach: function(context, settings) {
+			
+			// when the document is ready
+			$(document).ready(function(){
 
-			// select links that start with a hash tag
-			$('a[href^="#"]').click(function(event){
+				// select links that start with a hash tag
+				$('a[href^="#"]').click(function(event) {
 
-			// prevent default behavior
-			event.preventDefault();
+					// prevent default behavior
+					event.preventDefault();
 
-			// declare destination variable	
-			var destination = 0;
+					// declare destination variable	
+					var destination = 0;
 
-			// if the link is down the page
-			if($(this.hash).offset().top > $(document).height()-$(window).height()){
+					// if the link is down the page
+					if ( $(this.hash).offset().top > $(document).height() - $(window).height() ) {
 
-				// set destination to distance from the top of the window
-				destination = $(document).height()-$(window).height();
+						// set destination to distance from the top of the window
+						destination = $(document).height() - $(window).height();
 
-			// if the link is not down the page
-			} else {
+					// if the link is not down the page
+					} else {
 
-				// set the destination to the top of the page
-				destination = $(this.hash).offset().top;
+						// set the destination to the top of the page
+						destination = $(this.hash).offset().top;
 
-			}
+					}
 
-				// scroll to destination
-				$('html,body').animate({scrollTop:destination}, 1000,'swing');
+					// scroll to destination
+					$('html,body').animate({ scrollTop: destination }, 800, 'swing');
 
+				});
+				
 			});
-
+			
 		}
 	};
 }(jQuery));
-
