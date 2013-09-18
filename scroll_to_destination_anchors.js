f5c0bef81fd747180d661044c0af1591af5bd72d
Keegan Robinson <contact@keeganrobinson.com>
2013-09-18 17:18:24 -0400

    By keeganhr, Keegan: Fixing bug if href does not match element and adding anchor name functionality.

--- scroll_to_destination_anchors.js
+++ scroll_to_destination_anchors.js
@@ -2,19 +2,36 @@
 
 Drupal.behaviors.scrolltoanchors = {
   attach: function(context, settings) {
+    function validate(a) {
+      return /^#[a-z]{1}[a-z0-9_-]*$/i.test(a);
+    }
+    function scroll(a,b) {
+      if (a > b) {
+        destination = b;
+      } else {
+        destination = a;
+      }
+      $('html,body').animate({ scrollTop: destination }, 800, 'swing');
+    }
     $(document).ready(function(){
       $('a[href^="#"]').click(function(event) {
         event.preventDefault();
-        var destination = 0;
-        var hrefValue = $(this).attr('href').replace('#','');
-        if (hrefValue) {
-          if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
-            destination = $(document).height() - $(window).height();
-          } else {
-            destination = $(this.hash).offset().top;
+        var href_value = $(this).attr('href');
+        var window_height = $(window).height();
+        var document_height = $(document).height();
+        var height_difference = document_height - window_height;
+        if (validate(href_value)) {
+          if ($(href_value).length > 0) {
+            var link_offset = $(this.hash).offset().top;
+            scroll(link_offset, height_difference);
           }
-          $('html,body').animate({ scrollTop: destination }, 800, 'swing');
+          else if ($('a[name=' + href_value + ']')) {
+            var href_value = href_value.replace('#','');
+            var link_offset = $('a[name=' + href_value + ']').offset().top;
+            scroll(link_offset, height_difference);
+          }
         }
+
       });
     });
   }
