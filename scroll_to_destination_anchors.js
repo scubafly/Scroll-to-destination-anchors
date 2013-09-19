5dd14e6e8d5dfb77c189eeb07ea4e3a02f2db543
keeganhr <contact@keeganrobinson.com>
2013-09-19 07:36:32 -0400

    Issue #2092941 by keeganhr: Adding selector validation to prevent console errors.

--- scroll_to_destination_anchors.js
+++ scroll_to_destination_anchors.js
@@ -1,40 +1,35 @@
 (function($) {
-
 Drupal.behaviors.scrolltoanchors = {
   attach: function(context, settings) {
-    function validate(a) {
-      return /^#[a-z]{1}[a-z0-9_-]*$/i.test(a);
-    }
-    function scroll(a,b) {
-      if (a > b) {
-        destination = b;
-      } else {
-        destination = a;
-      }
-      $('html,body').animate({ scrollTop: destination }, 800, 'swing');
-    }
     $(document).ready(function(){
+      function validateSelector(a) {
+        return /^#[a-z]{1}[a-z0-9_-]*$/i.test(a);
+      }
+      function scrollToDestination(a,b) {
+        if (a > b) {
+          destination = b;
+        } else {
+          destination = a;
+        }
+        $('html,body').animate({ scrollTop: destination }, 800, 'swing');
+      }
       $('a[href^="#"]').click(function(event) {
         event.preventDefault();
-        var href_value = $(this).attr('href');
-        var window_height = $(window).height();
-        var document_height = $(document).height();
-        var height_difference = document_height - window_height;
-        if (validate(href_value)) {
-          if ($(href_value).length > 0) {
-            var link_offset = $(this.hash).offset().top;
-            scroll(link_offset, height_difference);
+        var hrefValue = $(this).attr('href');
+        var hrefValueStripped = hrefValue.replace('#','');
+        var heightDifference = $(document).height() - $(window).height();
+        if (validateSelector(hrefValue)) {
+          if ($(hrefValue).length > 0) {
+            var linkOffset = $(this.hash).offset().top;
+            scrollToDestination(linkOffset, heightDifference);
           }
-          else if ($('a[name=' + href_value + ']')) {
-            var href_value = href_value.replace('#','');
-            var link_offset = $('a[name=' + href_value + ']').offset().top;
-            scroll(link_offset, height_difference);
+          else if ($('a[name=' + hrefValueStripped + ']').length > 0) {
+            var linkOffset = $('a[name=' + hrefValueStripped + ']').offset().top;
+            scrollToDestination(linkOffset, heightDifference);
           }
         }
-
       });
     });
   }
 };
-
 }(jQuery));
